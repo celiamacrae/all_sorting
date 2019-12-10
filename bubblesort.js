0c9645a78dbfaec53500b430af65cb67c53dc106
lianach <liana.andreea97@yhaoo.com>
2019-12-10 15:05:27 -0500

    first commit

---
+++ bubblesort.js
@@ -0,0 +1,21 @@
+function bubbleSort(arr) {
+
+  let ok;
+
+  do{
+    ok=false;
+    for(let i=0;i<arr.length-1;i++){
+
+      if(arr[i]>arr[i+1]){
+        let temp=arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=temp;
+        ok=true;
+      }
+    }
+
+  }while(ok!==false)
+
+return arr;
+}
+console.log(bubbleSort([2,4,3,1,5]))
